c6ed16b50149fb9df8c0e111538105ceefbe3bbb
ganchaobo <ganchaobo@sailvan.com>
2017-05-02 15:45:59 +0800

    第一次提交

---
+++ firstTest/firstTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  firstTest
+//
+//  Created by gcb999 on 2017/5/2.
+//  Copyright © 2017年 gcb999. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
